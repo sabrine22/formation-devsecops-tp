9cb4fc85db3e415ccaea8d36f1f137ed8a31ef02
sabrine.fatnassi <sabrine.fatnassi@labom2iformation.fr>
2024-02-07 09:24:05 +0100

    a

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,7 +27,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Kubernetes DevSecOpssabrine";
 		}
 
 		@GetMapping("/compare/{value}")
